feb3b33ad2271dac569cda7692cc3a1bbb581748
Andres M Bran <doncamilom@gmail.com>
2023-06-07 14:25:57 +0200

    adding project structure

--- src/syn2act/__init__.py
+++ src/syn2act/__init__.py
@@ -3,3 +3,5 @@
 """Convert a synthetic procedure paragraph into a structured data object."""
 
 from .api import *  # noqa
+from .extract import *
+from .segment import *
---
+++ src/syn2act/extract/__init__.py
@@ -0,0 +1,4 @@
+from .rxn_setup import *
+from .rxn_workup import *
+from .purification import *
+from .analysis import *
