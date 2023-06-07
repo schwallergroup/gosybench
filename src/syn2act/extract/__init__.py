61eecd945e13b8756732bb49e741823234f17b94
Andres M Bran <doncamilom@gmail.com>
2023-06-07 16:29:31 +0200

    add docs for init + comment flake8 for now

--- src/syn2act/extract/__init__.py
+++ src/syn2act/extract/__init__.py
@@ -1,4 +1,7 @@
+"""Given a text segment and text class, extract relevant data into JSON format"""
+
+
+from .analysis import *
+from .purification import *
 from .rxn_setup import *
 from .rxn_workup import *
-from .purification import *
-from .analysis import *
--- src/syn2act/segment/__init__.py
+++ src/syn2act/segment/__init__.py
@@ -1,1 +1,3 @@
+"""Segment a synthethic procedure paragraph into semantically distinct pieces."""
+
 from .gpt import *
