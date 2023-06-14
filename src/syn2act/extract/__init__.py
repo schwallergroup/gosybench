7512ff67a04f432e3d0524e8417967ab6ae49384
lancelotzomax <a0953161159@gmail.com>
2023-06-14 17:59:47 +0200

    add code  on __init__.py and delete code on extract_utils

--- src/syn2act/extract/__init__.py
+++ src/syn2act/extract/__init__.py
@@ -1,7 +1,3 @@
 """Given a text segment and text class, extract relevant data into JSON format"""
 
-
-from .analysis import *
-from .purification import *
-from .rxn_setup import *
-from .rxn_workup import *
+from .extract_utils import *
--- src/syn2act/extract/extract_utils.py
+++
@@ -1,3 +0,0 @@
-"""Given a text segment and text class, extract relevant data into JSON format"""
-
-from .extract_utils import *
