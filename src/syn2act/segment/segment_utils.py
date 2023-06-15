657023d0f546524dc65b6029326c88a4ef446c35
lancelotzomax <a0953161159@gmail.com>
2023-06-15 18:56:18 +0200

    add a function paragraph2SegmentJson in segment package

--- src/syn2act/segment/segment_utils.py
+++ src/syn2act/segment/segment_utils.py
@@ -1,15 +1,17 @@
 """
 utilities for segmentation
 """
-
+import time
 import json
 import re
 
+from syn2act.segment.gpt import chain
+from syn2act.segment.prompt import example
 
 # segment paragraph texts into
 def paragraph2SegmentDict(paragraph):
     """
-    paragraph input will be segmented, together with text class, explanation and step order 
+    paragraph input will be segmented, together with text class, explanation and step order
     """
     output = []
     segments = re.split("Step end #", paragraph)  # split the paragraph text into segments by step
@@ -30,6 +32,7 @@ def paragraph2SegmentDict(paragraph):
         output.append(dict_temp)  # save the dictionary into the list
     return output
 
+
 # print output as csv structure
 def printOutput(output):
     """
@@ -37,3 +40,22 @@ def printOutput(output):
     """
 
     print(json.dumps(output, sort_keys=False, indent=3, ensure_ascii=False))
+
+
+def paragraph2SegmentJson(text):
+    """
+    Segment a synthesis description paragraph, using LLMs, into numerous segmented paragraph, together with class, explanation and step order.
+    Save these segments into a JSON .
+    """
+
+    # step 1
+    start_time = time.time()
+    segmented_paragraph = chain.run({"example": example, "paragraph": text})
+    # print(segmented_paragraph)
+    end_time = time.time()
+    # print("time: ", end_time - start_time)
+
+    paragraph = paragraph2SegmentDict(segmented_paragraph)
+    paragraph.pop()
+
+    printOutput(paragraph)
