f1bfa239d50726461e9da748d8a599ff02770ff4
lancelotzomax <a0953161159@gmail.com>
2023-06-15 16:02:59 +0200

    updated version of paragraph2json

--- src/syn2act/extract/extract_utils.py
+++ src/syn2act/extract/extract_utils.py
@@ -7,6 +7,7 @@ import time
 from syn2act.segment.gpt import chain
 from syn2act.segment.prompt import example
 from syn2act.segment.segment_utils import paragraph2SegmentDict
+from syn2act.segment.segment_utils import printOutput
 
 from .analysis import *
 from .purification import *
@@ -29,33 +30,36 @@ def paragraph2json(text):
     paragraph = paragraph2SegmentDict(segmented_paragraph)
     paragraph.pop()
 
-    # step 2
-    for i in range(len(paragraph)):
-        if paragraph[i]["text class"] == "reaction set-up":
-            output = chain_object.predict_and_parse(text=(paragraph[i]["text segment"]))["data"]
-            print(output, "\n\n")
-            # if (output):  # if the Kor-extracted data exist, append the segmented paragraph with the extracted-data
-            #     paragraph[i]['properties'] = output['properties']
+    printOutput(paragraph)
 
-        elif paragraph[i]["text class"] == "work-up":
-            output = chain_work_up.predict_and_parse(text=(paragraph[i]["text segment"]))["data"]
-            print(output, "\n\n")
-            # if (output):  # if the Kor-extracted data exist, append the segmented paragraph with the extracted-data
-            #     paragraph[i]['properties'] = output['properties']
 
-        elif paragraph[i]["text class"] == "purification":
-            output = chain_purification.predict_and_parse(text=(paragraph[i]["text segment"]))[
-                "data"
-            ]
-            print(output, "\n\n")
-            # if (output):  # if the Kor-extracted data exist, append the segmented paragraph with the extracted-data
-            #     paragraph[i]['properties'] = output['properties']
+    # # step 2
+    # for i in range(len(paragraph)):
+    #     if paragraph[i]["text class"] == "reaction set-up":
+    #         output = chain_object.predict_and_parse(text=(paragraph[i]["text segment"]))["data"]
+    #         print(output, "\n\n")
+    #         # if (output):  # if the Kor-extracted data exist, append the segmented paragraph with the extracted-data
+    #         #     paragraph[i]['properties'] = output['properties']
 
-        elif paragraph[i]["text class"] == "analysis":
-            output = chain_analysis.predict_and_parse(text=(paragraph[i]["text segment"]))["data"]
-            print(output, "\n\n")
-            # if (output):  # if the Kor-extracted data exist, append the segmented paragraph with the extracted-data
-            #     paragraph[i]['properties'] = output['properties']
+    #     elif paragraph[i]["text class"] == "work-up":
+    #         output = chain_work_up.predict_and_parse(text=(paragraph[i]["text segment"]))["data"]
+    #         print(output, "\n\n")
+    #         # if (output):  # if the Kor-extracted data exist, append the segmented paragraph with the extracted-data
+    #         #     paragraph[i]['properties'] = output['properties']
+
+    #     elif paragraph[i]["text class"] == "purification":
+    #         output = chain_purification.predict_and_parse(text=(paragraph[i]["text segment"]))[
+    #             "data"
+    #         ]
+    #         print(output, "\n\n")
+    #         # if (output):  # if the Kor-extracted data exist, append the segmented paragraph with the extracted-data
+    #         #     paragraph[i]['properties'] = output['properties']
+
+    #     elif paragraph[i]["text class"] == "analysis":
+    #         output = chain_analysis.predict_and_parse(text=(paragraph[i]["text segment"]))["data"]
+    #         print(output, "\n\n")
+    #         # if (output):  # if the Kor-extracted data exist, append the segmented paragraph with the extracted-data
+    #         #     paragraph[i]['properties'] = output['properties']
 
     # print(paragraph)
 
