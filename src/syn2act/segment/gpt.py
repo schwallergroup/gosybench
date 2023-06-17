12c51385261a6ad24c649ec6f1b53ebbf38f4ae3
Andres M Bran <doncamilom@gmail.com>
2023-06-17 17:58:07 +0200

    moved syn2segment to the segment module + formatting changes

--- src/syn2act/segment/gpt.py
+++ src/syn2act/segment/gpt.py
@@ -11,6 +11,7 @@ from langchain.chat_models import ChatOpenAI
 from .prompt import *
 
 # Load OPENAI API key
+load_dotenv()
 openai_key = os.getenv("OPENAI_API_KEY")
 
 
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,3 @@
 # -*- coding: utf-8 -*-
 
 """Tests for :mod:`syn2act`."""
-
