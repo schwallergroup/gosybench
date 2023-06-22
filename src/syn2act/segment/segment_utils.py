d6169f24015349393e7232b4c8026a08f0945ae1
lancelotzomax <a0953161159@gmail.com>
2023-06-22 14:05:54 +0200

    improvement by adding try-except clauses in _parse_llm_segm

--- src/syn2act/segment/segment_utils.py
+++ src/syn2act/segment/segment_utils.py
@@ -49,10 +49,18 @@ def _parse_llm_segm(llm_segm: str):
         for j in range(0, len(sentences)):
             item = sentences[j].split(": ")  # split label and its content
             #            print('item:', item)
-            if item[0] == "":  # continue if the label does not exist
-                continue
-            else:
-                dict_temp[item[0]] = item[1]  # save index and value in the dictionary
+            try: 
+                if (item[0] == "text segment" or item[0] == "text class" or item[0] == "explanation" or item[0] == "step order"):  # continue if the label does not exist
+                    dict_temp[item[0]] = item[1]  # save index and value in the dictionary
+                
+                else:
+                    continue
+            except:
+                if item[0] == "" :  # continue if the label does not exist
+                    continue
+
+                else:
+                    dict_temp[item[0]] = item[1]  # save index and value in the dictionary
 
         output.append(dict_temp)  # save the dictionary into the list
     return output
