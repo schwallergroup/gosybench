ffb63af223bcbadfa960a34a228c63bf76c95372
Andres M Bran <doncamilom@gmail.com>
2023-08-25 15:27:21 +0200

    last local changes, no idea

--- src/syn2act/__main__.py
+++ src/syn2act/__main__.py
@@ -8,7 +8,7 @@ Why does this file exist, and why ``__main__``? For more info, read:
 - https://docs.python.org/3/using/cmdline.html#cmdoption-m
 """
 
-from .cli import main, segment
+from .cli import main  # , segment
 
 if __name__ == "__main__":
     main()
