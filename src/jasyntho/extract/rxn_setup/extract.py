2d6ca9a2fafe046e2f9b0a56064c7cc4b6bcdf3a
Andres <doncamilom@gmail.com>
2023-11-24 17:21:51 +0100

    remove CDE, all in pydantic now + async implementation

--- src/jasyntho/extract/rxn_setup/extract.py
+++ src/jasyntho/extract/rxn_setup/extract.py
@@ -1,21 +1,12 @@
-"""Data extraction from reaction setup text segments"""
+"""Data extraction from reaction setup text segments."""
 
-import ast
-import os
-import re
-from typing import List, Tuple, Union
-
+from typing import Union
 import instructor  # type: ignore
-from chemdataextractor import Document  # type: ignore
-from chemdataextractor.doc import Heading  # type: ignore
 from dotenv import load_dotenv
-from langchain.chains import LLMChain
-from langchain.chat_models import ChatOpenAI
-from openai import OpenAI  # type: ignore
+from openai import OpenAI, AsyncOpenAI  # type: ignore
 from pydantic import ValidationError
 
-from .prompts import propextr_tpl
-from .typing import SubstanceList
+from .typing import Product
 
 load_dotenv()
 
@@ -26,163 +17,26 @@ class ReactionSetup:
     def __init__(self, api_key=None):
         """Initialize LLMChain for data extraction with GPT-4."""
         load_dotenv()
-        openai_key = api_key or os.environ.get("OPENAI_API_KEY")
 
-        llm = ChatOpenAI(
-            model_name="gpt-3.5-turbo",
-            openai_api_key=openai_key,
-            temperature=0.0,
-            max_tokens=512,
-            request_timeout=60,  # wait for max 1 min
-        )
-
-        self.llm = 'gpt-3.5-turbo'
+        self.llm = 'gpt-4-1106-preview'
+        # self.llm = 'gpt-3.5-turbo'
         self.client = instructor.patch(OpenAI())
-        self.prod_prop_chain = LLMChain(prompt=propextr_tpl, llm=llm)
+        self.aclient = instructor.apatch(AsyncOpenAI())
 
     def __call__(self, text: str) -> Union[dict, list]:
         """Execute the extraction pipeline for a single paragraph."""
         try:
-            parag, prods_md = self._products_metadata(text)
-            prods_child = self._extract_reacts(parag)
-
-            # Finally complete prods_md with prods_child
-            prod_list = []
-            for prod_key, prod_d in prods_md.items():
-                if prods_child["status"] == "success":
-                    prod_d["children"] = prods_child["data"]
-                elif len(prods_md.keys()) > 0:
-                    prod_d["children"] = []
-                prod_list.append(prod_d)
-
-            return prod_list
+            product = Product.from_paragraph(text, self.client, self.llm)
+            return product
         except ValidationError:
             return []
 
-    def _products_metadata(self, text: str) -> Tuple[str, dict]:
-        """Extract metadata for all products described in paragraph.
-
-        Input:
-            text : str
-                full text of paragr, containing prod name, id, rxn, etc.
-        Output:
-            dict: list of products with metadata + reaction set-up
-                follows schema:
-                {
-                    'prod_label':
-                        {
-                            'substance_name': '...',
-                            'reference_key': '...',
-                            'mass': '...',
-                            'yield': '...',
-                            'moles': '...',
-                        }
-                }
-        """
-        header, content = self._split_paragraph(text)
-        products = self._extract_prods_header(header)
-        segments = self._filter_sgmnt(content)
-
-        prods_inp = str([p["labels"] for p in products])
-        prod_props = self._get_prod_data(prods_inp, segments)
-        merged_data = self._merge_prod_data(products, prod_props)
-        return content, merged_data
-
-    def _extract_reacts(self, prg: str) -> dict:
-        """Extract the substances in a reaction."""
-        # TODO: this is only a proxy to getting reaction setup.
-        prg = prg[:500]
-
-        subs = self.client.chat.completions.create(
-            model=self.llm,
-            response_model=SubstanceList,
-            messages=[
-                {"role": "user", "content": prg},
-            ],
-            temperature=0.2,
-            max_retries=2,
-        )
-
-        # Wrap into dict with error status
+    async def async_call(self, text: str) -> Union[dict, list]:
+        """Execute the extraction pipeline for a paragraph asynchronously."""
         try:
-            subs = self.__clean_synth_dict(subs)
-            return {"status": "success", "data": subs}
+            product = Product.async_from_paragraph(
+                text, self.aclient, self.llm
+            )
+            return await product
         except ValidationError:
-            return {"status": "failure", "data": None}
-        return subs
-
-    def __clean_synth_dict(self, slist: SubstanceList) -> List[dict]:
-        """Clean extracted json of paragraph children."""
-        # Add keys if None
-        for s in slist.substances:
-            if s.reference_key is None:
-                s.reference_key = s.substance_name
-
-        # keep only 'reactant' and 'solvent'
-        new_subs = SubstanceList()
-        for h in slist.substances:
-            if h.role in ["reactant", "solvent"]:
-                new_subs.substances.append(h)
-
-        # Keep only unique keys
-        uniq_d = {d.reference_key: d for d in reversed(new_subs.substances)}
-        subs = [s.model_dump() for s in uniq_d.values()]  # type: ignore
-        return subs
-
-    def _merge_prod_data(self, products, prod_props):
-        """Merge property and product ID dictionaries."""
-        f_data = {}
-        for p in products:
-            lbl = p["labels"][0]
-            try:
-                names = p["names"]  # assume 'names' exists
-            except KeyError:
-                names = lbl
-
-            f_data[lbl] = {"substance_name": names, "reference_key": lbl}
-            f_data[lbl].update(prod_props[lbl])
-        return f_data
-
-    def _split_paragraph(self, text: str) -> tuple:
-        """Split paragraph into header and content."""
-        try:
-            # Find occurence of (ID), where ID is product identifier:
-            # any combination of letters and numbers
-            match = re.search(r"\([A-Za-z\d]+\)\:", text)
-            sp = match.span()  # type: ignore
-
-            header = text[: sp[1]]
-            parag = text[sp[1] :]
-        except AttributeError:
-            header = ""
-            parag = text
-        return header, parag
-
-    def _extract_prods_header(self, head):
-        """Extract product name and label from header using CDE."""
-        prg = Document(Heading(head))
-        return prg.records.serialize()
-
-    def _filter_sgmnt(self, prg):
-        """Get relevant excerpts from paragraph.
-
-        Return concat excerpts that may contain
-        analytical data from products
-        """
-        segments = ""
-        for i, m in enumerate(re.finditer(r"yield|\%", prg)):
-            s = m.span()
-            segments += f"{i+1}. {prg[s[0]-60:s[1]+60]}\n"
-        return segments
-
-    def _get_prod_data(self, products, segments):
-        """Get analytical data for product.
-
-        TODO: mod with instructor
-        """
-        response = self.prod_prop_chain.run(products=products, segments=segments)
-        try:
-            prod_props = ast.literal_eval(response)
-        except SyntaxError:
-            prod_props = []
-        return prod_props
+            return []
--- src/jasyntho/extract/rxn_setup/typing.py
+++ src/jasyntho/extract/rxn_setup/typing.py
@@ -1,6 +1,7 @@
-"""Typing for the OpenAI API."""
+"""pydantic models for the substance classes."""
 
-from typing import List, Literal, Optional
+import instructor
+from typing import List, Literal, Optional, Dict, Union
 from pydantic import BaseModel, Field
 
 
@@ -8,7 +9,7 @@ class Substance(BaseModel):
     """A substance in a reaction."""
 
     reference_key: Optional[str] = Field(
-        description=("Identifier for a substance described in paragraph. "),
+        description=("Identifier for a substance described in text. "),
     )
     substance_name: str = Field(
         description="Name of the substance.",
@@ -20,6 +21,13 @@ class Substance(BaseModel):
             "steps of the reaction, such as washing, quenching, etc."
         )
     )
+
+    @classmethod
+    def from_lm(cls, s):
+        """Reconvert into Substance. Fill reference_key if missing."""
+        if s.reference_key is None:
+            s.reference_key = s.substance_name
+        return cls(**s.model_dump())
 
 
 class SubstanceList(BaseModel):
@@ -33,3 +41,90 @@ class SubstanceList(BaseModel):
     """
 
     substances: List[Substance] = Field(..., default_factory=list)
+
+
+class Product(Substance):
+    """Substance with children and properites.
+
+    Args:
+        reference_key: Identifier for a substance described in text.
+        substance_name: Name of the substance.
+        children: List of substances that are children of this substance.
+        props: Dictionary of properties of this substance.
+    """
+
+    role: str = 'product'  # type: ignore
+    children: List[Substance]
+    props: Optional[Dict[str, str]] = None
+
+    @classmethod
+    def from_substancelist(cls, slist: SubstanceList):
+        """Convert from SubstanceList."""
+        child = {}
+        pkey = ''
+        pname = ''
+        for s in slist.substances:
+            if s.role == 'product':
+                pname = s.substance_name
+                pkey = s.reference_key
+                if pkey is None:
+                    pkey = pname
+            else:
+                # TODO: handle multiple children
+                child[s.reference_key] = Substance.from_lm(s)
+        return cls(
+            reference_key=pkey,
+            substance_name=pname,
+            children=list(child.values())
+        )
+
+    @classmethod
+    def from_paragraph(
+            cls,
+            prgr: str,
+            client: instructor.patch,
+            llm: str
+    ):
+        """Extract the substances in a reaction."""
+        prgr = prgr[:700]
+
+        try:
+            subs_list = client.chat.completions.create(
+                model=llm,
+                response_model=SubstanceList,
+                messages=[
+                    {"role": "user", "content": prgr},
+                ],
+                temperature=0.2,
+                max_retries=2,
+                timeout=60,
+            )
+            return cls.from_substancelist(subs_list)
+        except:
+            return cls()
+
+
+    @classmethod
+    async def async_from_paragraph(
+            cls,
+            prgr: str,
+            aclient: instructor.apatch,
+            llm: str
+    ):
+        """Extract the substances in a reaction."""
+        prgr = prgr[:700]
+
+        # try:
+        subs_list = await aclient.chat.completions.create(
+            model=llm,
+            response_model=SubstanceList,
+            messages=[
+                {"role": "user", "content": prgr},
+            ],
+            temperature=0.2,
+            max_retries=2,
+            timeout=60,
+        )
+        return cls.from_substancelist(subs_list)
+        #except:
+        #    return cls()
