02f9bf0cafb80e84d34b3f0cd9998cc002d4f7c9
Andres <doncamilom@gmail.com>
2024-04-16 15:02:15 +0200

    add mistral to models for instructor

--- src/jasyntho/extract/rxns_si/extract.py
+++ src/jasyntho/extract/rxns_si/extract.py
@@ -2,6 +2,7 @@
 
 from typing import List
 
+import os
 import instructor  # type: ignore
 from dotenv import load_dotenv
 from openai import AsyncOpenAI, OpenAI  # type: ignore
@@ -17,8 +18,7 @@ class ReactionSetup:
         load_dotenv()
 
         self.llm = model
-        self.client = instructor.patch(OpenAI())
-        self.aclient = instructor.apatch(AsyncOpenAI())
+        self.set_llm(model)
 
     def __call__(self, text: str) -> List[Product]:
         """Execute the extraction pipeline for a single paragraph."""
@@ -31,3 +31,32 @@ class ReactionSetup:
             text, self.aclient, self.llm
         )
         return product
+
+    def set_llm(self, model: str):
+        """Set the language model to be used."""
+
+        if model.startswith("gpt"):
+            self.client = instructor.patch(OpenAI())
+            self.aclient = instructor.apatch(AsyncOpenAI())
+
+        elif model.startswith("mistral"):
+            url = "https://api.mistral.ai/v1/"
+            api_key = os.getenv("MISTRAL_API_KEY")
+
+            self.client = instructor.patch(
+                OpenAI(base_url=url, api_key=api_key),
+                mode=instructor.Mode.JSON
+            )
+            self.aclient = instructor.apatch(
+                AsyncOpenAI(base_url=url, api_key=api_key),
+                mode=instructor.Mode.JSON
+            )
+
+        elif model.startswith("claude"):
+            self.client = instructor.patch(OpenAI(base_url="https://api.anthropic.com/v1/messages", api_key=os.getenv("ANTHROPIC_API_KEY")))
+            self.aclient = instructor.apatch(AsyncOpenAI(base_url="https://api.anthropic.com/v1/messages", api_key=os.getenv("ANTHROPIC_API_KEY")))
+
+        else:
+            raise ValueError(f"Model {model} not recognized.")
+
+
