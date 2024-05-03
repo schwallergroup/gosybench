087f3512d9008cd4f9dba95a38ac7fa898a26658
Andres M Bran <doncamilom@gmail.com>
2024-05-03 13:55:03 +0200

    some change

--- scripts/all_papers.py
+++ scripts/all_papers.py
@@ -7,54 +7,21 @@ import wandb
 from jasyntho.api import SynthesisExtract
 from jasyntho.metrics import TreeMetrics
 
-llm_list = [
-    "gpt-3.5-turbo",
-    "gpt-4-turbo",
-    "gpt-4-0613",
-    "gpt-4-turbo-2024-04-09",
-    "claude-3-haiku-20240307",
-    "claude-3-sonnet-20240229",
-    "claude-3-opus-20240229",
-    "mistral-small-latest",
-    "mistral-large-latest",
-    "mistral-medium-latest",
-    "open-mixtral-8x7b",
-    "open-mixtral-8x22b",
-]
 
-papers = [
-    "jacs.0c00969",
-    "jacs.0c05479",
-    "jacs.1c01356",
-    "jacs.0c07397",
-    "jacs.1c01372",
-    "ja074300t",
-    "jacs.0c13424",
-    "jacs.0c10053",
-    "jacs.0c06354",
-    "jacs.0c02513",
-    "angewandte_01",
-    "jacs.0c11025",
-    "jacs.0c07433",
-    "jacs.0c09520",
-    "jacs.1c00457",
-    "jacs.1c01135",
-]
-
-
-def run(inst_model, dspy_model, paper):
+def run(inst_model, dspy_model_1, dspy_model_2, paper):
 
     # Initialize stuff
-    synthex = SynthesisExtract(inst_model=inst_model, dspy_model=dspy_model)
+    synthex = SynthesisExtract(inst_model=inst_model, dspy_model_1=dspy_model_1, dspy_model_2=dspy_model_2)
     metrics = TreeMetrics()
 
     # Init before to keep track of time
     wandb.init(
-        project="jasyntho-routes",
+        project="jasyntho-all-jacs",
         config=dict(
             paper=paper.strip("/").split("/")[-1],
             start_model=inst_model,
-            dspy_model=dspy_model,
+            dspy_model_1=dspy_model_1,
+            dspy_model_2=dspy_model_2,
         ),
     )
 
@@ -70,16 +37,40 @@ def run(inst_model, dspy_model, paper):
 
 @click.command()
 @click.option(
-    "--llm",
+    "--llm1",
     default="gpt-3.5-turbo",
-    type=click.Choice(llm_list),
+    type=str,
     help="LLM to use for paragraph processing (can be async).",
 )
-def main(llm):
+@click.option(
+    "--llm2",
+    default="gpt-3.5-turbo",
+    type=str,
+    help="LLM for extra connection finding",
+)
+@click.option(
+    "--llm3",
+    default="gpt-3.5-turbo",
+    type=str,
+    help="LLM for recovering IUPAC names",
+)
+@click.option(
+    "--dir_",
+    default=".",
+    type=click.Path(exists=True),
+    help="Path to directory containing papers.",
+)
+def main(llm1, llm2, llm3, dir_):
+
+    papers = os.listdir(dir_)
+    print(papers)
+
     for p in papers:
-        plink = os.path.join("../../data/", p)
+        plink = os.path.join(dir_, p)
+        print(plink)
         try:
-            run(inst_model=llm, dspy_model=llm, paper=plink)
+            run(inst_model=llm1, dspy_model_1=llm2, dspy_model_2=llm3, paper=plink)
+            print(f"Succesfully ran {plink}")
         except:
             continue
 
