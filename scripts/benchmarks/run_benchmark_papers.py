05ed07bdbf9f816fc5fd36f14df205407a51e6be
Andres M Bran <doncamilom@gmail.com>
2024-05-20 18:44:23 +0200

    update results from runs + script

--- scripts/benchmarks/run_benchmark_papers.py
+++ scripts/benchmarks/run_benchmark_papers.py
@@ -1,27 +1,28 @@
 """Run all benchmarks on multiple models."""
 
-import os
 import asyncio
-import wandb
+import os
 import pickle
+
+import wandb
 from jasyntho import SynthTree
 from jasyntho.extract import ExtractReaction
 from jasyntho.metrics import TreeMetrics
 
-
-
 # For an input paper, run extraction using various methods, log each
 
-async def extractg(path, model='gpt-3.5-turbo', method='text', si_select=False):
 
+async def extractg(
+    path, model="gpt-3.5-turbo", method="text", si_select=False
+):
 
     wandb.init(
         project="graphex-benchmark",
         config=dict(
-            paper=path.split('/')[-1],
+            paper=path.split("/")[-1],
             product_model=model,
             parsing_method=method,
-            si_select=si_select,
+            si_select=siuselect,
         ),
     )
 
@@ -31,17 +32,21 @@ async def extractg(path, model='gpt-3.5-turbo', method='text', si_select=False):
         tree = SynthTree.from_dir(path)
         tree.rxn_extract = ExtractReaction(llm=model)
 
-        tree.raw_prods = await tree.async_extract_rss(mode=method, si_select=si_select)
+        tree.raw_prods = await tree.async_extract_rss(
+            mode=method, si_select=si_select
+        )
         tree.products = [p for p in tree.raw_prods if not p.isempty()]
-        extracted_G = tree.full_g
+        tree.full_g = tree.get_full_graph(tree.products)
+        tree.reach_subgraphs = tree.get_reach_subgraphs(tree.full_g)
 
-        if model=='gpt-3.5-turbo':
+
+        if model == "gpt-3.5-turbo":
             k = "gpt35"
-        elif model=='gpt-4-turbo':
+        elif model == "gpt-4-turbo":
             k = "gpt4t"
-        elif model=='gpt-4':
+        elif model == "gpt-4":
             k = "gpt4"
-        elif model=='gpt-4o':
+        elif model == "gpt-4o":
             k = "gpt4o"
 
         if si_select:
@@ -49,56 +54,61 @@ async def extractg(path, model='gpt-3.5-turbo', method='text', si_select=False):
         else:
             si = ""
 
-        with open(os.path.join(path, f'extracted_graph_{k}_{method}_{si}.pickle'), 'wb') as f:
-            pickle.dump(extracted_G, f)
+        with open(
+            os.path.join(path, f"extracted_graph_{k}_{method}_{si}.pickle"),
+            "wb",
+        ) as f:
+            pickle.dump(tree.full_g, f)
 
         # Calc descriptors for extracted graph
         m = metrics(tree)
         print(f"CELA {m}")
-        wandb.summary.update(dict(**m, success= True))
-    except:
+        wandb.summary.update(dict(**m, success=True))
+    except Exception as e:
+        print(e)
         wandb.summary.update({"success": False})
     wandb.finish()
 
-    return extracted_G
-
+    # return tree.full_g
 
 async def main(path):
-    for method in ['text', 'vision']:
-        for model in ['gpt-3.5-turbo', 'gpt-4-turbo', 'gpt-4o']:
+    for method in ["vision", "text"]:
+        for model in ["gpt-3.5-turbo", "gpt-4-turbo"]:
             for si_select in [True, False]:
+                og = await extractg(
+                    path, model=model, method=method, si_select=si_select
+                )
 
-                if model=='gpt-3.5-turbo':
-                    k = "gpt35"
-                elif model=='gpt-4-turbo':
-                    k = "gpt4t"
-                elif model=='gpt-4':
-                    k = "gpt4"
-                elif model=='gpt-4o':
-                    k = "gpt4o"
+                # if model == "gpt-3.5-turbo":
+                #     k = "gpt35"
+                # elif model == "gpt-4-turbo":
+                #     k = "gpt4t"
+                # elif model == "gpt-4":
+                #     k = "gpt4"
+                # elif model == "gpt-4o":
+                #     k = "gpt4o"
 
-                if si_select:
-                    si = "_select"
-                else:
-                    si = ""
-                og = await extractg(path, model=model, method=method, si_select=si_select)
+                # if si_select:
+                #     si = "_select"
+                # else:
+                #     si = ""
                 # with open(os.path.join(path, f'extracted_graph_{k}_{method}{si}.pickle'), 'rb') as f:
                 #     og = pickle.load(f)
                 #     print(k, method, si)
+
 
 if __name__ == "__main__":
 
     papers = [
         "jacs.0c00363",
         "ja074300t",
-        "ja512124c",
         "jacs.0c00308",
         "jacs.0c02143",
         "jacs.0c02513",
         "jacs.1c01356",
         "jacs.1c00293",
         "jacs.3c01991",
-        "jacs.3c07019",
+        "jacs.3c07019",  # left to finish
         "jacs.8b00148",
         "jacs.7b13260",
         "jacs.7b09929",
@@ -123,4 +133,3 @@ if __name__ == "__main__":
     for p in papers:
         path = f"benchmark/papers/{p}"
         asyncio.run(main(path))
-        
