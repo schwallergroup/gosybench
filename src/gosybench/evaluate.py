637f7b1b5966aa9ecb2b59257fdd11faa57a1c1a
Andres M Bran <doncamilom@gmail.com>
2024-12-02 16:22:50 +0100

    format

--- src/gosybench/evaluate.py
+++ src/gosybench/evaluate.py
@@ -1,28 +1,33 @@
 """Class for evaluating the performance of a model."""
 
-from pydantic import BaseModel
 from typing import Callable, List
-from typing import List
-from task import Task, _load_default_tasks
+
 from logger import setup_logger
+from metrics import TreeMetrics
+from pydantic import BaseModel
+from task import Task, _load_default_tasks
 
 logger = setup_logger()
 
 
 class GOSyBench(BaseModel):
     tasks: List[Task] = _load_default_tasks()
+    metrics: TreeMetrics = TreeMetrics()
 
     def evaluate(self, f: Callable):
         """Run the evaluation."""
         logger.info("Running evaluation")
         for task in self.tasks:
             logger.info(f"Running task {task}")
+            self.metrics(task.tree)
             task.run(f)
 
 
 if __name__ == "__main__":
+    gosybench = GOSyBench()
+
     import networkx as nx
 
-    gosybench = GOSyBench()
     method = lambda x: nx.DiGraph()
+
     gosybench.evaluate(method)
